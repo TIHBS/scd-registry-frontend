74f42fe47153328edecd77751870fbd2a9fb9fa4
GarondEisenfaust <chr.schr@web.de>
2022-04-04 15:44:35 +0200

    moves the header bar to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">👀</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/register">Register</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/scds">SCDs</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/settings">Settings</router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <header-bar></header-bar>
   <br />
   <div class="viewport"><router-view /></div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import HeaderBar from "@/components/HeaderBar.vue";
 
-export default defineComponent({ components: {} });
+export default defineComponent({ components: { HeaderBar } });
 </script>
 
 <style>
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">👀</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/register">Register</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/scds">SCDs</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/settings">Settings</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({ components: {} });
+</script>
+<style></style>
