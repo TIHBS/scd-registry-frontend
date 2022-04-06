cf664766699a90c6bcc5f633b4f65c1b9dbc701c
GarondEisenfaust <chr.schr@web.de>
2022-04-06 17:29:33 +0200

    adds spinner and moves files

--- src/components/MetamaskConnect.vue
+++ src/components/MetamaskConnect.vue
@@ -1,5 +1,6 @@
 <template>
   <span v-if="isLoggedIn" class="navbar-text">{{ computedAddress }}</span>
+  <div v-else-if="waiting" class="spinner-border" role="status"></div>
   <div v-else>
     <button
       v-if="isMetamaskSupported"
@@ -19,20 +20,26 @@ import {
   checkIfMetamaskIsInstalled,
   connectMetamask,
   truncateAddress,
-} from "@/metamask/Metamask";
+} from "@/ethereum/Metamask";
 
 const isMetamaskSupported = ref(false);
 const signer = ref<Signer>();
 const address = ref("");
 const isLoggedIn = ref(false);
-
+const waiting = ref(false);
 onMounted(
   () => (isMetamaskSupported.value = checkIfMetamaskIsInstalled(window))
 );
 
 async function connectWallet() {
-  [signer.value, address.value] = await connectMetamask(window);
-  isLoggedIn.value = address.value != "";
+  connectMetamask(window)
+    .then((result) => {
+      [signer.value, address.value] = result;
+      isLoggedIn.value = address.value != "";
+    })
+    .catch((err) => {})
+    .finally(() => (waiting.value = false));
+  waiting.value = true;
 }
 
 const computedAddress = computed(() => truncateAddress(address.value));
