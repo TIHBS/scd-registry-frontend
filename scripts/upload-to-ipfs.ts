9e26ce1ebe08502c2fb005c5fe2f6d25291e5b8e
GarondEisenfaust <chr.schr@web.de>
2022-04-25 19:58:13 +0200

    dockerized the frontend

---
+++ scripts/upload-to-ipfs.ts
@@ -0,0 +1,24 @@
+import { create, globSource, CID } from "ipfs-http-client";
+
+async function progressFunction(bytes: number, path?: string) {
+  console.info(`Byte: ${bytes} ${path ? path : ""}`);
+}
+
+async function main() {
+  let gateway = process.env.IPFS_GATEWAY
+    ? process.env.IPFS_GATEWAY
+    : "http://0.0.0.0:5001";
+
+  const ipfs = await create({ url: new URL(gateway) });
+
+  let cid: CID;
+  for await (const file of ipfs.addAll(globSource("dist", "**/*"), {
+    pin: true,
+    wrapWithDirectory: true,
+  })) {
+    cid = file.cid;
+    const path = file.path.length != 0 ? file.path : "the frontend at";
+    console.log(`Access ${path} at: https://ipfs.io/ipfs/${cid}`);
+  }
+}
+main().then().catch();
