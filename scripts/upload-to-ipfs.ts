f481d1ac2add5bf6bf11bb4ad64e0d7f62728b64
GarondEisenfaust <chr.schr@web.de>
2022-04-26 20:05:13 +0200

    fixes log

--- scripts/upload-to-ipfs.ts
+++ scripts/upload-to-ipfs.ts
@@ -17,7 +17,7 @@ async function main() {
     wrapWithDirectory: true,
   })) {
     cid = file.cid;
-    const path = file.path.length != 0 ? file.path : "the frontend at";
+    const path = file.path.length != 0 ? file.path : "the frontend";
     console.log(`Access ${path} at: https://ipfs.io/ipfs/${cid}`);
   }
 }
