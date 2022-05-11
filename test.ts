c66b6c9dfcbb70e54a5e876d34282c441ba44ff6
GarondEisenfaust <chr.schr@web.de>
2022-05-11 17:36:20 +0200

    removes the file called test

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { PowergateConnector } from "./bla";
-
-async function main() {
-  const powergateConnector = new PowergateConnector();
-  await powergateConnector.init();
-
-  await powergateConnector.upload("./test.ts");
-}
-main().then().catch();
